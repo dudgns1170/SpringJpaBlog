f76d33fe3d9fa15c91be77f8fd83e61b136af2b9
dudgns1170 <yh1170@naver.com>
2023-01-09 16:22:13 +0900

    Jpa Blog

--- src/main/java/com/yh/blog/model/RoleType.java
+++
@@ -1,5 +0,0 @@
-package com.yh.blog.model;
-
-public enum RoleType {
-	USER, ADMIN
-}
--- src/main/java/com/yh/blog/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yh.blog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yh.blog.model.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-
-}
